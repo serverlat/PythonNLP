3b9ec849acf4975c94d29882b507523fbcf340fe
Olga <olgasa@stud.ntnu.no>
2021-03-06 15:47:28 +0100

    Finished 2?

---
+++ Lab4/Lab4_Ex2.py
@@ -0,0 +1,36 @@
+import nltk 
+import re
+
+def chunk(sentence):
+    grammar = r"""
+    NP: {<DT>? <JJ>* <NN>*} 
+    NN: {<NN> <NN>}
+    NNP: {<NNP>*}"""
+    parser = nltk.RegexpParser(grammar)
+    result = parser.parse(sentence)
+    return result
+
+text = []
+
+with open("Lab4/SpaceX.txt", "r") as f:
+    for line in f.readlines():
+        if line != "\n": 
+            temp = line.strip()
+            text.append(re.findall(r"[\w']+", temp)) # filtering out words only, but it messes up [s]illiest :(
+
+tagged = []
+
+for line in text:
+    tagged.append(nltk.pos_tag(line))
+       
+labels = ["NP", "NN", "NNP"] 
+
+for i in range(5):
+    tree = chunk(tagged[i])
+    for subtree in tree.subtrees():
+        if subtree.label() in labels:
+            print("Chunk:", subtree)
+            words = [leaf[0] for leaf in subtree.leaves()]
+            print("Matching text:"," ".join(words), "\n")
+
+    
