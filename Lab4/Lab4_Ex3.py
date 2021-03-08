218a1e68d81af92ef25b26ec05bcac1d1930bc55
olgasa <olgasa@stud.ntnu.no>
2021-03-08 21:51:31 +0100

    Finished 4?

--- Lab4/Lab4_Ex3.py
+++ Lab4/Lab4_Ex3.py
@@ -5,7 +5,7 @@ dog = ["this", "dog", "runs"]
 dogs = ["these", "dogs", "run"]
 nonsense = ["there", "is", "cat"]
 
-grammar1 = nltk.CFG.fromstring("""
+grammar = nltk.CFG.fromstring("""
     S -> NP V
     NP -> Det N
     Det -> "this" | "these" 
@@ -13,8 +13,8 @@ grammar1 = nltk.CFG.fromstring("""
     V -> "run" | "runs" 
 """)
 
-def parse_sent(sentence):
-    parser = nltk.RecursiveDescentParser(grammar1)
+def parse(sentence):
+    parser = nltk.RecursiveDescentParser(grammar)
     try:
         trees = list(parser.parse(sentence))
         for tree in trees:
@@ -24,20 +24,6 @@ def parse_sent(sentence):
         print("Invalid syntax :(")
 
 
-parse_sent(nonsense)
-
-grammar2 = """
-    % start S
-    S -> NP[NUM=?n] V[NUM=?n]
-    NP[NUM=?n] -> Det[NUM=?n] N[NUM=?n]
-    Det[NUM=sg] -> "this" 
-    Det[NUM=pl] -> "these"
-    N[NUM=sg] -> "dog"
-    N[NUM=pl] -> "dogs" 
-    V[NUM=sg] -> "runs" 
-    V[NUM=pl] -> "run" 
-"""
-grammar3 = grammar.FeatureGrammar.fromstring(grammar2)
-parser2 = parse.FeatureEarleyChartParser(grammar3)
-trees = parser2.parse(dog)
-for tree in trees: print(tree)
+parse(nonsense)
+parse(dog)
+parse(dogs)
